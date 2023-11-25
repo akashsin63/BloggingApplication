242bf15efbc1af8df8053e80cfe98d234848a5f8
Akash <akashsingh54812@gmail.com>
2023-11-25 22:17:47 +0530

    POST API created

--- blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
+++ blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
@@ -1,5 +1,30 @@
 package com.blogging.blog.controllers;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.blogging.blog.payloads.UserDTO;
 
+import com.blogging.blog.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	//POST -> creat user
+	//PUT -> update user
+	//DELETE -> delete 
+	// GET  --> get user / get all user
+	@Autowired
+	private UserService userService;
+	
+	//POST 
+	@GetMapping("/")
+	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDto){
+		UserDTO createUserDto = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
 }
