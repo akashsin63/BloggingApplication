30e602e74b75cf36376f6b542ab01665ff8490eb
Akash <akashsingh54812@gmail.com>
2023-11-25 23:19:56 +0530

    Post api commit after testing

--- blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
+++ blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class UserController {
 	private UserService userService;
 	
 	//POST 
-	@GetMapping("/")
+	@PostMapping("/")
 	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDto){
 		UserDTO createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
