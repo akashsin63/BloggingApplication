07c768f0201334933d304f8f28b75cda63e30348
Akash <akashsingh54812@gmail.com>
2023-11-26 14:57:11 +0530

    final commit for APi

--- blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
+++ blog-app-api/src/main/java/com/blogging/blog/controllers/UserController.java
@@ -1,14 +1,23 @@
 package com.blogging.blog.controllers;
 
 import com.blogging.blog.services.UserService;
+
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.blogging.blog.payloads.ApiResponse;
 import com.blogging.blog.payloads.UserDTO;
 
 import com.blogging.blog.services.UserService;
@@ -29,5 +38,33 @@ public class UserController {
 	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDto){
 		UserDTO createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
+	
+	
+	//POST
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO , @PathVariable Integer userId){
+		UserDTO updatUser = this.userService.updateUser(userDTO, userId);
+		
+		return ResponseEntity.ok(updatUser);
+	}
+	
+	//DELETE
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser( @PathVariable Integer userId){
+		this.userService.deleteUser(userId);
+		return new ResponseEntity(new ApiResponse("User deleted successfully" , true),HttpStatus.OK);
+	}
+	
+	
+	//GET(All, and 
+	@GetMapping("/")
+	public ResponseEntity<List<UserDTO>> getAllUsers(){
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDTO> getAllUsers(@PathVariable Integer userId){
+		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
 }
--- blog-app-api/src/main/java/com/blogging/blog/payloads/ApiResponse.java
+++ blog-app-api/src/main/java/com/blogging/blog/payloads/ApiResponse.java
@@ -1,5 +1,15 @@
 package com.blogging.blog.payloads;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+	private String message;
+	private boolean status;
 }
